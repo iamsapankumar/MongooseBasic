db275e838ce77d182d47017b72641403b937eece
Sapan Kumar Das <sapanindia@gmail.com>
2018-03-07 15:03:57 +0530

    Removing Data

--- app.js
+++ app.js
@@ -27,14 +27,52 @@ const Car = mongoose.model('Car', carSchema)
 })
 addCar.save((err, doc)=>{
     if(err) return console.log(err)
-    console.log(doc)
+    console.log(doc);
 })*/
 /*
 Car.find((err, doc)=>{
     if(err) return console.log(err);
     console.log(doc)
 })*/
+/*
 Car.find({brand:"BMW"},(err, doc)=>{
     if(err) return console.log(err);
     console.log(doc)
+})*/
+//using id
+/*Car.find({_id:"5a9fa4288ad319259c62d1c2"},(err, doc)=>{
+    if(err) return console.log(err)
+    console.log(doc)
+})*/
+//findOne functon
+/*Car.findOne({brand:"BMW"}, (err, doc)=>{
+    if(err) return console.log(err)
+    console.log(doc)
+})*/
+/* findById */
+/*Car.findById("5a9fa4288ad319259c62d1c2",(err, doc)=>{
+
+    if(err) return console.log(err)
+    console.log(doc)
+})*/
+/*How to Removing Data*/ 
+//findOneAndRemove
+//findByIdAndRemove
+//remove
+
+/*Car.findOneAndRemove({brand:"BMW"},(err, doc)=>{   //only one document will be removed
+    if(err) return console.log(err)
+    console.log(doc)
+})*/
+/*
+Car.findByIdAndRemove("5a9f9788be0e5c12905fbd84",(err, doc)=>{ //pass the Id
+    if(err) return console.log(err)
+    console.log(doc)
+})*/
+
+Car.remove({brand:"BMW"}, (err, doc)=>{   
+    if(err)  return console.log(err)
+    console.log(doc)
 })
+
+
